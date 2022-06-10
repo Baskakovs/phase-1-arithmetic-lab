23ee81456a5faa23a1cbb76184a58282437c52df
Pjotrs Baskakovs <baskakovs@me.com>
2022-06-10 11:18:10 +0300

    commit tree dba9f28f363d0f4e3a14577e03e3e645e1a12191

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+const num1 = 8
+const num2 = 8
+const multiply = (num1*num2)-2
+
+const random = Math.floor((Math.random() * 100) + 1);
+
+const num3=44
+const num4 = 10
+
+const mod = (num3 % num4)
+
+const max =  Math.max(8, 3, 4, 2, 6, 20);
